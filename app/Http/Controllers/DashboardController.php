8c4e5b1da375ff887c6bdd4c891e6bf840c72ca1
root <narulhidayah9923@gmail.com>
2019-12-24 15:16:38 +0700

    chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -21,18 +21,14 @@ class DashboardController extends Controller
     public function index()
     {
         $user_id = Auth::user()->id;
-
-        $data['total_subscribers'] = \App\Subscribers::where('user_id', $user_id)
-            ->count();
         
         $data['product'] = \App\User::join('subscribers', 'subscribers.user_id', '=', 'users.id')
             ->join('products', 'products.product_id', '=', 'users.product_id')
             ->where('user_id', $user_id)->first();
 
+        $data['total_subscribers'] = \App\Subscribers::where('user_id', $user_id)->count();
         $data['total_landingpage'] = \App\Landingpage::where('user_id', $user_id)->count();
-
         $data['activity_log'] = \App\Subscribers::where('user_id', $user_id)->get();
-        // var_dump($data['activity_log']);die();
         
         if (Auth::check() && Auth::user()->level == 'admin') {
             return $this->dashboard();
@@ -50,25 +46,21 @@ class DashboardController extends Controller
             ->join('products', 'products.product_id', '=', 'users.product_id')
             ->where('user_id', $user_id)->first();
 
-        $data['total_landingpage'] = \App\Landingpage::get()->count();
         $data['activity_log'] = \App\Subscribers::where('user_id', $user_id)->get();
 
+        // chart jumlah total landing page
+        $data['total_landingpage'] = \App\Landingpage::get()->count();
         // Chart jumlah users
         $data['total_users'] = \App\User::where('level','<>','admin')->count();
         // Chart user aktif, kadaluarsa, non aktif
         $data['users_aktif'] = \App\User::where('status','valid')->count();
-        $data['users_kadaluarsa'] = \App\User::where('masa_aktif','<=',$time_now);
-        var_dump($data['users_kadaluarsa']);die();
+        $data['users_kadaluarsa'] = \App\User::where('masa_aktif','<=',$time_now)->where('level','<>','admin')->count();
+        $data['users_nonaktif'] = \App\User::where('status','<>','valid')->where('level','<>','admin')->count();
+        // Chart Payment & Leads
+        $data['total_active'] = \App\Order::where('order_status','Active')->count();
+        $data['total_pending'] = \App\Order::where('order_status','Pending')->count();
+        $data['total_expired'] = \App\Order::where('order_status','Expired')->count();
+        
         return view('dashboard.dashboard', $data);
-    }
-
-    
-    public function fecth_promo()
-    {
-        // Fetch all records
-        $promo['data'] = \App\Promo::orderBy('created_at', 'desc')->limit(5)->get();
-
-        echo json_encode($promo);
-        exit;
     }
 }
--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -22,6 +22,148 @@
 <!-- Main content -->
 <section class="content">
   <div class="container-fluid">
+    <!-- Small boxes (Stat box) -->
+    @if(Auth::user()->status == "valid" || Auth::user()->status == "Valid")
+    <div class="row">
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+          <div class="inner">
+            <h3>{{ $total_users }}<sup style="font-size: 20px"></sup><span style="font-size:20px;"></span></h3>
+
+            <p>Total Users</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-users"></i>
+          </div>
+          <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+          <div class="inner">
+            <h3>{{ $total_landingpage}}</h3>
+
+            <p>Landing Page</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-bars"></i>
+          </div>
+          <a href="{{ route('landingpages.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+          <div class="inner">
+            <h3>44</h3>
+
+            <p>Total Share</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-paper-plane"></i>
+          </div>
+          <a href="{{ url('report/share') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+      <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+          <div class="inner">
+            <h3>65</h3>
+
+            <p>Total Visitor</p>
+          </div>
+          <div class="icon">
+            <i class="fas fa-chart-pie"></i>
+          </div>
+          <a href="{{ url('report/trafik') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+      </div>
+      <!-- ./col -->
+    </div>
+    <!-- /.row -->
+    <!-- Main row -->
+    <div class="row">
+      <!-- Left col -->
+      <section class="col-lg-6 connectedSortable">
+        
+        {{-- card --}}
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              <i class="fas fa-chart-pie mr-1"></i>
+              Chart Users
+            </h3>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-8">
+                <div class="chart-responsive">
+                  <canvas id="users-chart" height="250"></canvas>
+                </div>
+                <!-- ./chart-responsive -->
+              </div>
+              <!-- /.col -->
+              <div class="col-md-4">
+                <ul class="chart-legend clearfix">
+                  <li><i class="far fa-circle text-success"></i> Users Aktif: {{$users_aktif}}</li>
+                  <li><i class="far fa-circle text-info"></i> Users Kadaluarsa: {{$users_kadaluarsa}}</li>
+                  <li><i class="far fa-circle text-danger"></i> Users Nonaktif: {{$users_nonaktif}}</li>
+                </ul>
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div><!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+      </section>
+      <!-- /.Left col -->
+      <!-- right col (We are only adding the ID to make the widgets sortable)-->
+      <section class="col-lg-6 connectedSortable">
+
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">
+              <i class="fas fa-chart-pie mr-1"></i>
+              Payment & Leads
+            </h3>
+          </div><!-- /.card-header -->
+          <div class="card-body">
+            <div class="row">
+              <div class="col-md-8">
+                <div class="chart-responsive">
+                  <canvas id="payment-chart" height="250"></canvas>
+                </div>
+                <!-- ./chart-responsive -->
+              </div>
+              <!-- /.col -->
+              <div class="col-md-4">
+                <ul class="chart-legend clearfix">
+                  <li><i class="far fa-circle text-success"></i> Active: {{$total_active}} </li>
+                  <li><i class="far fa-circle text-info"></i> Pending: {{$total_pending}}</li>
+                  <li><i class="far fa-circle text-danger"></i> Expired: {{$total_expired}}</li>
+                </ul>
+              </div>
+              <!-- /.col -->
+            </div>
+            <!-- /.row -->
+          </div><!-- /.card-body -->
+        </div>
+        <!-- /.card -->
+
+
+        
+      </section>
+      <!-- right col -->
+    </div>
+    @else
     <div class="row">
       <div class="col-12">
         <div class="card">
@@ -40,9 +182,72 @@
         </div>
       </div>
     </div>
-    
+    @endif
+    <!-- /.row (main row) -->
   </div><!-- /.container-fluid -->
 </section>
 <!-- /.content -->
 
+<script>
+
+$(function () {
+
+  var pieChartCanvas = $('#payment-chart').get(0).getContext('2d')
+    var pieData        = {
+      labels: [
+          'Active',
+          'Pending',
+          'Expired',
+      ],
+      datasets: [
+        {
+          data: [{{$total_active}},{{$total_pending}},{{$total_expired}}],
+          backgroundColor : ['#00a65a', '#00c0ef', '#f56954'],
+        }
+      ]
+    }
+    var pieOptions     = {
+      legend: {
+        display: false
+      }
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'doughnut',
+      data: pieData,
+      options: pieOptions      
+    })
+
+    // users chart
+
+    var pieChartCanvas = $('#users-chart').get(0).getContext('2d')
+    var pieData        = {
+      labels: [
+          'Jumlah User Aktif',
+          'Kadaluarsa',
+          'Jumlah User Non Aktif'
+      ],
+      datasets: [
+        {
+          data: [{{$users_aktif}},{{$users_kadaluarsa}},{{$users_nonaktif}}],
+          backgroundColor : ['#00a65a', '#00c0ef', '#f56954'],
+        }
+      ]
+    }
+    var pieOptions     = {
+      legend: {
+        display: false
+      }
+    }
+    //Create pie or douhnut chart
+    // You can switch between pie and douhnut using the method below.
+    var pieChart = new Chart(pieChartCanvas, {
+      type: 'doughnut',
+      data: pieData,
+      options: pieOptions      
+    })
+
+});
+</script>
 @endsection
